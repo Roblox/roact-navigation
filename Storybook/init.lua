4393cecf53484ff9488f61da713387a2aa64e101
Anonymous Author <roflection-bot@roblox.com>
2019-08-13 14:45:49 -0700

    Make Roact-Navigation use Rotriever for runtime dependencies (#25)
    Pull in @chafke's fork which converts Roact-Navigation to use Rotriever. I will update the docs and fix any stray file format issues in a follow-up change.

--- Storybook/init.lua
+++ Storybook/init.lua
@@ -1,6 +1,4 @@
-local ReplicatedStorage = game:getService("ReplicatedStorage")
-
 return {
 	name = "Roact-Navigation Storybook",
-	storyRoot = ReplicatedStorage.RoactNavigationStorybook,
+	storyRoot = script,
 }
