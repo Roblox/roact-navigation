fc6fbbab0e3ebe7e52f7c856a35d9489e5430313
Anonymous Author <roflection-bot@roblox.com>
2019-08-22 15:29:49 -0700

    Update code docs for StackPresentationStyle
    This change is a simple codedoc update to reflect the current thinking
    and implementation for stack views.

--- src/views/StackView/StackPresentationStyle.lua
+++ src/views/StackView/StackPresentationStyle.lua
@@ -6,35 +6,38 @@ local OVERLAY_SYMBOL = NavigationSymbol("OVERLAY")
 
 --[[
 	StackPresentationStyle is used with stack navigators/views to determine
-	the behavior of a given view when it is pushed/popped from the stack, as
-	well as the visual effects applied while the view is on screen.
+	the behavior of a given screen when it is pushed/popped from the stack, as
+	well as the visual effects/transitions applied while the view is on screen.
 ]]
 return {
 	--[[
-		The Default presentation style follows the expected behavior of the host
-		operating system. For example, on iOS, views will slide in and out from
-		the right side; on Android, they will pop on and off screen. No special
-		visual effects are applied to default views; they fill the entire space
-		available and are always opaque.
+		The Default presentation style draws stack cards so that they will
+		slide in and out from the right side of the screen one at a time. No
+		special visual effects are applied, and cards always fill the entire space
+		available. Your screen content is rendered over an opaque background color
+		by default, but you have the option to draw the cards with a semi- or fully
+		transparent background via navigationOptions. Cards always prevent
+		tap-through of the content underneath them (in case your navigation
+		container is transparent).
 	]]
 	Default = DEFAULT_SYMBOL,
 
 	--[[
-		The Modal presentation style causes the screen to animate according to
-		modal behaviors for the host operating system. Modals are allowed to
-		have transparency and they apply a darkening and blur to the underlying
-		content. They are always presented full-screen and will obscure other
-		visual elements even if they are declared within a navigator that
-		occupies a sub-area of another screen.
+		The Modal presentation style causes screens to animate up/down from the
+		bottom of the navigation container and visually stack on top of each
+		other. Cards are opaque by default, but you may set navigationOptions
+		to make them semi- or fully transparent so that you can see the underlying
+		cards. Modal cards always prevent tap-through of any underlying UI, including
+		other cards in the same stack.
 	]]
 	Modal = MODAL_SYMBOL,
 
 	--[[
-		The Overlay presentation style causes the view to be displayed full-screen
-		on top of other visual elements much like for modals. The key difference is
-		that overlays pop in/out instead of animating in from the bottom, and they
-		do not blur the underlying content. This makes them ideal for tool-tips and
-		informative pop-ups (e.g. toasts).
+		The Overlay presentation style causes screens to pop in (later they will fade in)
+		on top of the underlying screens. Like modals, they visually stack on top of each
+		other. Cards are opaque by default, but you may set navigationOptions to make
+		them semi- or fully transparent. Overlay cards always prevent tap-through of any
+		underlying UI, including other cards in the same stack.
 	]]
 	Overlay = OVERLAY_SYMBOL
 }
