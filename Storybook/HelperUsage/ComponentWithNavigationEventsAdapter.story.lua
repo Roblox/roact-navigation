6695d0c48c30385e07647b9d5c527f8f270d0d71
Anonymous Author <roflection-bot@roblox.com>
2019-08-27 17:19:02 -0700

    Implement story showing EventsAdapter
    This change implements a story showing how to use EventsAdapter, and
    fixes some minor naming/formatting in createAppContainer (no functional
    change).

---
+++ Storybook/HelperUsage/ComponentWithNavigationEventsAdapter.story.lua
@@ -0,0 +1,166 @@
+local Roact = require(script.Parent.Parent.Parent.Roact)
+local RoactNavigation = require(script.Parent.Parent.Parent.RoactNavigation)
+
+--[[
+	This story demonstrates how to build a screen component that listens
+	to navigation events using RoactNavigation.EventsAdapter. It makes a
+	basic master-detail stack navigator to exercise the events.
+]]
+return function(target)
+	local MasterPage = Roact.Component:extend("MasterPage")
+
+	function MasterPage:init()
+		self._onWillFocus = function()
+			print("MasterPage: willFocus")
+			self:setState({
+				bgColor = Color3.new(1, 0, 0),
+			})
+		end
+
+		self._onWillBlur = function()
+			print("MasterPage: willBlur")
+			self:setState({
+				bgColor = Color3.new(0, 0, 1),
+			})
+		end
+
+		self._onDidFocus = function()
+			print("MasterPage: didFocus")
+			self:setState({
+				bgColor = Color3.new(0, 1, 0),
+			})
+		end
+
+		self._onDidBlur = function()
+			print("MasterPage: didBlur")
+			self:setState({
+				bgColor = Color3.new(0, 0, 0),
+			})
+		end
+
+		self.state = {
+			bgColor = Color3.new(1, 1, 1),
+		}
+	end
+
+	function MasterPage:didMount()
+		print("MasterPage: didMount")
+	end
+
+	function MasterPage:willUnmount()
+		print("MasterPage: willUnmount")
+	end
+
+	function MasterPage:render()
+		local navigation = self.props.navigation
+		local bgColor = self.state.bgColor
+
+		return Roact.createElement("TextLabel", {
+			Size = UDim2.new(1, 0, 1, 0),
+			BackgroundColor3 = bgColor,
+			BorderSizePixel = 0,
+			Font = Enum.Font.Gotham,
+			Text = "Master Page",
+			TextColor3 = Color3.new(0, 0, 0),
+			TextSize = 18,
+		}, {
+			eventsAdapter = Roact.createElement(RoactNavigation.EventsAdapter, {
+				[RoactNavigation.Events.WillFocus] = self._onWillFocus,
+				[RoactNavigation.Events.DidFocus] = self._onDidFocus,
+				[RoactNavigation.Events.WillBlur] = self._onWillBlur,
+				[RoactNavigation.Events.DidBlur] = self._onDidBlur,
+			}),
+			detailButton = Roact.createElement("TextButton", {
+				AnchorPoint = Vector2.new(0.5, 0),
+				BackgroundColor3 = Color3.new(1, 1, 1),
+				Font = Enum.Font.Gotham,
+				Position = UDim2.new(0.5, 0, 0.6, 0),
+				Size = UDim2.new(0, 160, 0, 30),
+				Text = "Go to Detail",
+				TextColor3 = Color3.new(0, 0, 0),
+				TextSize = 18,
+				[Roact.Event.Activated] = function()
+					-- Note that you can push() to force a new instance, instead!
+					navigation.navigate("Detail")
+				end
+			})
+		})
+	end
+
+	local DetailPage = Roact.Component:extend("DetailPage")
+
+	function DetailPage:init()
+		self._onWillFocus = function()
+			print("DetailPage: willFocus")
+			self:setState({
+				bgColor = Color3.new(1, 0, 0),
+			})
+		end
+
+		self._onWillBlur = function()
+			print("DetailPage: willBlur")
+			self:setState({
+				bgColor = Color3.new(0, 0, 1),
+			})
+		end
+
+		self._onDidFocus = function()
+			print("DetailPage: didFocus")
+			self:setState({
+				bgColor = Color3.new(0, 1, 0),
+			})
+		end
+
+		self._onDidBlur = function()
+			print("DetailPage: didBlur")
+			self:setState({
+				bgColor = Color3.new(0, 0, 0),
+			})
+		end
+
+		self.state = {
+			bgColor = Color3.new(0, 0, 0),
+		}
+	end
+
+	function DetailPage:didMount()
+		print("DetailPage: didMount")
+	end
+
+	function DetailPage:willUnmount()
+		print("DetailPage: willUnmount")
+	end
+
+	function DetailPage:render(props)
+		local bgColor = self.state.bgColor
+
+		return Roact.createElement("Frame", {
+			Size = UDim2.new(1, 0, 1, 0),
+			BackgroundColor3 = bgColor,
+		}, {
+			eventsAdapter = Roact.createElement(RoactNavigation.EventsAdapter, {
+				[RoactNavigation.Events.WillFocus] = self._onWillFocus,
+				[RoactNavigation.Events.DidFocus] = self._onDidFocus,
+				[RoactNavigation.Events.WillBlur] = self._onWillBlur,
+				[RoactNavigation.Events.DidBlur] = self._onDidBlur,
+			}),
+		})
+	end
+
+	-- This is the top-level navigator. Note that child navigators are just Roact
+	-- Components attached like any other route.
+	local rootNavigator = RoactNavigation.createTopBarStackNavigator({
+		routes = {
+			Master = MasterPage,
+			Detail = DetailPage,
+		},
+		initialRouteName = "Master",
+	})
+
+	local appContainer = RoactNavigation.createAppContainer(rootNavigator)
+	local rootInstance = Roact.mount(Roact.createElement(appContainer), target)
+
+	return function()
+		Roact.unmount(rootInstance)
+	end
+end
