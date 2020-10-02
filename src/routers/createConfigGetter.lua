e2d8beb70aa30c0c1d6e87aaae397583cbbf794b
Anonymous Author <roflection-bot@roblox.com>
2020-10-02 13:25:35 -0700

    align createConfigGetter with upstream (#50)

--- src/routers/createConfigGetter.lua
+++ src/routers/createConfigGetter.lua
@@ -1,3 +1,4 @@
+-- upstream https://github.com/react-navigation/react-navigation/blob/6390aacd07fd647d925dfec842a766c8aad5272f/packages/core/src/routers/createConfigGetter.js
 local Cryo = require(script.Parent.Parent.Parent.Cryo)
 local getScreenForRouteName = require(script.Parent.getScreenForRouteName)
 local validateScreenOptions = require(script.Parent.validateScreenOptions)
@@ -8,10 +9,12 @@ local function applyConfig(configurer, navigationOptions, configProps)
 
 	local configurerType = type(configurer)
 	if configurerType == "function" then
-		return Cryo.Dictionary.join(navigationOptions,
-			configurer(Cryo.Dictionary.join(configProps or {}, {
+		return Cryo.Dictionary.join(
+			navigationOptions,
+			configurer(Cryo.Dictionary.join(configProps, {
 				navigationOptions = navigationOptions
-			})))
+			}))
+		)
 	elseif configurerType == "table" then
 		return Cryo.Dictionary.join(navigationOptions, configurer)
 	else
@@ -25,7 +28,10 @@ return function(routeConfigs, navigatorScreenConfig)
 		local route = navigation.state
 
 		validate(type(route) == "table", "navigation.state must be a table")
-		validate(type(route.routeName == "string"), "routeName must be a string")
+		validate(
+			type(route.routeName == "string"),
+			"Cannot get config because the route does not have a routeName."
+		)
 
 		local component = getScreenForRouteName(routeConfigs, route.routeName)
 		local routeConfig = routeConfigs[route.routeName]
@@ -35,12 +41,15 @@ return function(routeConfigs, navigatorScreenConfig)
 			routeScreenConfig = routeConfig.navigationOptions
 		end
 
+		-- deviation: check if the component is a table, because it could be a
+		-- function and it can't be indexed in Lua.
 		local componentScreenConfig = type(component) == "table"
 			and component.navigationOptions or {}
 
 		local configOptions = {
 			navigation = navigation,
 			screenProps = screenProps,
+			-- deviation: no theme support
 		}
 
 		local outputConfig = applyConfig(navigatorScreenConfig, {}, configOptions)
