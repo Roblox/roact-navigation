bb4f3381eeed01814f590eb09631fefdc5d1e558
Anonymous Author <roflection-bot@roblox.com>
2020-10-05 08:53:21 -0700

    Align validateRouteConfigMap with upstream (#52)

--- src/routers/_tests_/validateRouteConfigMap.spec.lua
+++ src/routers/_tests_/validateRouteConfigMap.spec.lua
@@ -2,94 +2,74 @@ return function()
 	local Roact = require(script.Parent.Parent.Parent.Parent.Roact)
 	local validateRouteConfigMap = require(script.Parent.Parent.validateRouteConfigMap)
 
-	local function expectError(functor, msg)
-		local status, err = pcall(functor)
-		expect(status).to.equal(false)
-		expect(string.find(err, msg)).to.never.equal(nil)
-	end
-
 	local TestComponent = Roact.Component:extend("TestComponent")
 	function TestComponent:render()
 		return nil
 	end
 
+	local INVALID_COMPONENT_MESSAGE = "The component for route 'myRoute' must be a Roact" ..
+		" component or table with 'getScreen'."
 
 	it("should throw if routeConfigs is not a table", function()
-		expectError(function()
+		expect(function()
 			validateRouteConfigMap(5)
-		end, "routeConfigs must be a table")
+		end).to.throw("routeConfigs must be a table")
 	end)
 
 	it("should throw if routeConfigs is empty", function()
-		expectError(function()
+		expect(function()
 			validateRouteConfigMap({})
-		end, "Please specify at least one route when configuring a navigator%.")
+		end).to.throw("Please specify at least one route when configuring a navigator.")
 	end)
 
 	it("should throw if routeConfigs contains an invalid Roact element", function()
-		expectError(function()
+		expect(function()
 			validateRouteConfigMap({
 				myRoute = 5,
 			})
-		end, "The component for route 'myRoute' must be a Roact Function/Stateful component or table with 'getScreen'." ..
-			"getScreen function must return Roact Function/Stateful component.")
-	end)
-
-	it("should throw if getScreen returns invalid Roact element", function()
-		expectError(function()
-			validateRouteConfigMap({
-				myRoute = {
-					getScreen = function() end,
-				},
-			})
-		end, "The component for route 'myRoute' must be a Roact Function/Stateful component or table with 'getScreen'." ..
-			"getScreen function must return Roact Function/Stateful component.")
+		end).to.throw()
 	end)
 
 	it("should throw when both screen and getScreen are provided for same component", function()
-		expectError(function()
+		expect(function()
 			validateRouteConfigMap({
 				myRoute = {
 					screen = "TheScreen",
 					getScreen = function() return TestComponent end,
 				}
 			})
-		end, "Route 'myRoute' should provide 'screen' or 'getScreen', but not both%.")
+		end).to.throw("Route 'myRoute' should declare a screen or a getScreen, not both.")
 	end)
 
-	it("should throw for a simple table where screen is not a Roact Function/Stateful component", function()
-		expectError(function()
+	it("should throw for a simple table where screen is not a Roact component", function()
+		expect(function()
 			validateRouteConfigMap({
 				myRoute = {
 					screen = {},
 				}
 			})
-		end, "The component for route 'myRoute' must be a Roact Function/Stateful component or table with 'getScreen'." ..
-			"getScreen function must return Roact Function/Stateful component.")
+		end).to.throw(INVALID_COMPONENT_MESSAGE)
 	end)
 
 	it("should throw for a non-function getScreen", function()
-		expectError(function()
+		expect(function()
 			validateRouteConfigMap({
 				myRoute = {
 					getScreen = 5
 				}
 			})
-		end, "The component for route 'myRoute' must be a Roact Function/Stateful component or table with 'getScreen'." ..
-			"getScreen function must return Roact Function/Stateful component.")
+		end).to.throw(INVALID_COMPONENT_MESSAGE)
 	end)
 
 	it("should throw for a Host Component", function()
-		expectError(function()
+		expect(function()
 			validateRouteConfigMap({
 				myRoute = {
 					aFrame = "Frame"
 				}
 			})
-		end, "The component for route 'myRoute' must be a Roact Function/Stateful component or table with 'getScreen'." ..
-			"getScreen function must return Roact Function/Stateful component.")
+		end).to.throw(INVALID_COMPONENT_MESSAGE)
 	end)
-
 
 	it("should pass for valid basic routeConfigs", function()
 		validateRouteConfigMap({
--- src/routers/validateRouteConfigMap.lua
+++ src/routers/validateRouteConfigMap.lua
@@ -1,5 +1,19 @@
-local validate = require(script.Parent.Parent.utils.validate)
-local isValidScreenComponent = require(script.Parent.Parent.utils.isValidScreenComponent)
+-- upstream https://github.com/react-navigation/react-navigation/blob/6390aacd07fd647d925dfec842a766c8aad5272f/packages/core/src/routers/validateRouteConfigMap.js
+local root = script.Parent.Parent
+local validate = require(root.utils.validate)
+local isValidScreenComponent = require(root.utils.isValidScreenComponent)
+
+local function getScreenComponent(routeConfig)
+	if not routeConfig then
+		return nil
+	end
+
+	if type(routeConfig) == "table" and routeConfig.screen then
+		return routeConfig.screen
+	end
+
+	return routeConfig
+end
 
 --[[
 	This utility checks to make sure that configs passed to a
@@ -21,24 +35,42 @@ local isValidScreenComponent = require(script.Parent.Parent.utils.isValidScreenC
 ]]
 return function(routeConfigs)
 	validate(type(routeConfigs) == "table", "routeConfigs must be a table")
+	validate(next(routeConfigs) ~= nil, "Please specify at least one route when configuring a navigator.")
 
-	local atLeastOne = false
 	for routeName, routeConfig in pairs(routeConfigs) do
-		local configIsTable = type(routeConfig) == "table" or false
-		local screenConfig = configIsTable and routeConfig or {} -- easy index .screen/.getScreen
-		local screenComponent = configIsTable and routeConfig.screen or routeConfig
-		validate(isValidScreenComponent(screenComponent) or
-			(type(screenConfig.getScreen) == "function" and isValidScreenComponent(screenConfig.getScreen())),
-			"The component for route '%s' must be a Roact Function/Stateful component or table with 'getScreen'." ..
-			"getScreen function must return Roact Function/Stateful component.",
-			routeName)
+		local screenComponent = getScreenComponent(routeConfig)
 
-		validate(screenConfig.screen == nil or screenConfig.getScreen == nil,
-			"Route '%s' should provide 'screen' or 'getScreen', but not both.", routeName)
-		atLeastOne = true
+		local tableRouteConfig = type(routeConfig) == "table"
+		validate(
+			isValidScreenComponent(screenComponent) or
+				(tableRouteConfig and type(routeConfig.getScreen) == "function"),
+			"The component for route '%s' must be a Roact component or table with 'getScreen'." ..
+				[[ For example:
+
+local MyScreen = require(script.Parent.MyScreen)
+...
+%s = MyScreen,
+}
+
+You can also use a navigator:
+
+local MyNavigator = require(script.Parent.MyNavigator)
+...
+%s = MyNavigator,
+}]],
+			routeName,
+			routeName,
+			routeName
+		)
+
+		if tableRouteConfig then
+			validate(
+				routeConfig.screen == nil or routeConfig.getScreen == nil,
+				"Route '%s' should declare a screen or a getScreen, not both.",
+				routeName
+			)
+		end
 	end
-
-	validate(atLeastOne, "Please specify at least one route when configuring a navigator.")
 
 	return routeConfigs
 end
