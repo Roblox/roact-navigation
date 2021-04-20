9f73660837c79d0021c945802e5a5ea56c0c66e1
Anonymous Author <roflection-bot@roblox.com>
2021-04-20 07:39:51 -0700

    fix scene reducer brittle tests (#80)
    Closes MOBLUAPP-2849

--- src/views/RobloxStackView/_tests_/ScenesReducer.spec.lua
+++ src/views/RobloxStackView/_tests_/ScenesReducer.spec.lua
@@ -1,6 +1,11 @@
 return function()
-	-- ScenesReducer(scenes, nextState, prevState, descriptors)
-	local ScenesReducer = require(script.Parent.Parent.ScenesReducer)
+	local RobloxStackViewModule = script.Parent.Parent
+	local viewsModule = RobloxStackViewModule.Parent
+	local RoactNavigationModule = viewsModule.Parent
+	local Packages = RoactNavigationModule.Parent
+	local jestExpect = require(Packages.Dev.JestRoblox).Globals.expect
+
+	local ScenesReducer = require(RobloxStackViewModule.ScenesReducer)
 
 	local initialRouteKey = "id-1"
 	local initialRouteName = "First Route"
@@ -28,17 +33,16 @@ return function()
 		},
 	}
 
-	local initialScenes = nil
-
 	it("should generate valid initial scene", function()
 		local scenes = ScenesReducer({}, initialState, nil, initialDescriptors)
-		local sceneOne = scenes[1]
-		expect(#scenes).to.be.equal(1)
-		expect(sceneOne.route).to.be.equal(initialRoute)
-		expect(sceneOne.index).to.be.equal(1)
-		expect(sceneOne.isActive).to.be.equal(true)
-		expect(sceneOne.isStale).to.be.equal(false)
-		initialScenes = scenes
+		jestExpect(scenes).toEqual({
+			jestExpect.objectContaining({
+				route = initialRoute,
+				index = 1,
+				isActive = true,
+				isStale = false,
+			})
+		})
 	end)
 
 	it("should update descriptor", function()
@@ -46,22 +50,23 @@ return function()
 		local dummyDescriptor = { key = "this is a dummy descriptor" }
 		scenes[1].descriptor = dummyDescriptor
 		local updatedScenes = ScenesReducer(scenes, initialState, initialState, initialDescriptors)
-		expect(#updatedScenes).to.be.equal(1)
-		local sceneOne = updatedScenes[1]
-		expect(sceneOne.descriptor).to.never.be.equal(dummyDescriptor)
-		expect(sceneOne.descriptor).to.be.equal(initialDescriptors[initialRouteKey])
-		expect(sceneOne.route).to.be.equal(initialRoute)
-		expect(sceneOne.index).to.be.equal(1)
-		expect(sceneOne.isActive).to.be.equal(true)
-		expect(sceneOne.isStale).to.be.equal(false)
+		jestExpect(updatedScenes).toEqual({
+			jestExpect.objectContaining({
+				descriptor = initialDescriptors[initialRouteKey],
+				route = initialRoute,
+				index = 1,
+				isActive = true,
+				isStale = false,
+			})
+		})
 	end)
 
 	it("should bail out early", function()
-		expect(initialScenes).to.never.be.equal(nil)
+		local initialScenes = ScenesReducer({}, initialState, nil, initialDescriptors)
 		local scenes = ScenesReducer(initialScenes, nil, nil, nil)
-		expect(scenes).to.be.equal(initialScenes)
+		jestExpect(scenes).toBe(initialScenes)
 		scenes = ScenesReducer(initialScenes, initialState, initialState, initialDescriptors)
-		expect(scenes).to.be.equal(initialScenes)
+		jestExpect(scenes).toBe(initialScenes)
 	end)
 
 	local secondRouteKey =  "id-2"
@@ -92,27 +97,26 @@ return function()
 		},
 	}
 
-	local secondScenes = nil
-
 	it("should add second scene", function()
-		expect(initialScenes).to.never.be.equal(nil)
+		local initialScenes = ScenesReducer({}, initialState, nil, initialDescriptors)
 		local scenes = ScenesReducer(initialScenes, secondState, initialState, secondDescriptors)
 		expect(scenes).to.never.be.equal(initialScenes)
 		expect(#scenes).to.be.equal(2)
 
-		local sceneOne = scenes[1]
-		expect(sceneOne.route).to.be.equal(initialRoute)
-		expect(sceneOne.index).to.be.equal(1)
-		expect(sceneOne.isActive).to.be.equal(false)
-		expect(sceneOne.isStale).to.be.equal(false)
-
-		local sceneTwo = scenes[2]
-		expect(sceneTwo.route).to.be.equal(secondRoute)
-		expect(sceneTwo.index).to.be.equal(2)
-		expect(sceneTwo.isActive).to.be.equal(true)
-		expect(sceneTwo.isStale).to.be.equal(false)
-
-		secondScenes = scenes
+		jestExpect(scenes).toEqual({
+			jestExpect.objectContaining({
+				route = initialRoute,
+				index = 1,
+				isActive = false,
+				isStale = false,
+			}),
+			jestExpect.objectContaining({
+				route = secondRoute,
+				index = 2,
+				isActive = true,
+				isStale = false,
+			}),
+		})
 	end)
 
 	local thirdRouteKey =  "id-3"
@@ -145,58 +149,61 @@ return function()
 		},
 	}
 
-	local thirdScenes = nil
-
 	it("should add third scene", function()
-		expect(initialScenes).to.never.be.equal(nil)
+		local initialScenes = ScenesReducer({}, initialState, nil, initialDescriptors)
+		local secondScenes = ScenesReducer(initialScenes, secondState, initialState, secondDescriptors)
+
 		local scenes = ScenesReducer(secondScenes, thirdState, secondState, thirdDescriptors)
-		expect(scenes).to.never.be.equal(initialScenes)
-		expect(#scenes).to.be.equal(3)
 
-		local sceneOne = scenes[1]
-		expect(sceneOne.route).to.be.equal(initialRoute)
-		expect(sceneOne.index).to.be.equal(1)
-		expect(sceneOne.isActive).to.be.equal(false)
-		expect(sceneOne.isStale).to.be.equal(false)
-
-		local sceneTwo = scenes[2]
-		expect(sceneTwo.route).to.be.equal(secondRoute)
-		expect(sceneTwo.index).to.be.equal(2)
-		expect(sceneTwo.isActive).to.be.equal(false)
-		expect(sceneTwo.isStale).to.be.equal(false)
-
-		local sceneThree = scenes[3]
-		expect(sceneThree.route).to.be.equal(thirdRoute)
-		expect(sceneThree.index).to.be.equal(3)
-		expect(sceneThree.isActive).to.be.equal(true)
-		expect(sceneThree.isStale).to.be.equal(false)
-
-		thirdScenes = scenes
+		jestExpect(scenes).toEqual({
+			jestExpect.objectContaining({
+				route = initialRoute,
+				index = 1,
+				isActive = false,
+				isStale = false,
+			}),
+			jestExpect.objectContaining({
+				route = secondRoute,
+				index = 2,
+				isActive = false,
+				isStale = false,
+			}),
+			jestExpect.objectContaining({
+				route = thirdRoute,
+				index = 3,
+				isActive = true,
+				isStale = false,
+			}),
+		})
 	end)
 
 	it("should mark removed scenes as stale", function()
-		expect(secondState).to.never.be.equal(nil)
+		local initialScenes = ScenesReducer({}, initialState, nil, initialDescriptors)
+		local secondScenes = ScenesReducer(initialScenes, secondState, initialState, secondDescriptors)
+		local thirdScenes = ScenesReducer(secondScenes, thirdState, secondState, thirdDescriptors)
+
 		local scenes = ScenesReducer(thirdScenes, initialState, thirdState, initialDescriptors)
-		expect(scenes).to.never.be.equal(initialScenes)
-		expect(#scenes).to.be.equal(3)
 
-		local sceneOne = scenes[1]
-		expect(sceneOne.route).to.be.equal(initialRoute)
-		expect(sceneOne.index).to.be.equal(1)
-		expect(sceneOne.isActive).to.be.equal(true)
-		expect(sceneOne.isStale).to.be.equal(false)
-
-		local sceneTwo = scenes[2]
-		expect(sceneTwo.route).to.be.equal(secondRoute)
-		expect(sceneTwo.index).to.be.equal(2)
-		expect(sceneTwo.isActive).to.be.equal(false)
-		expect(sceneTwo.isStale).to.be.equal(true)
-
-		local sceneThree = scenes[3]
-		expect(sceneThree.route).to.be.equal(thirdRoute)
-		expect(sceneThree.index).to.be.equal(3)
-		expect(sceneThree.isActive).to.be.equal(false)
-		expect(sceneThree.isStale).to.be.equal(true)
+		jestExpect(scenes).toEqual({
+			jestExpect.objectContaining({
+				route = initialRoute,
+				index = 1,
+				isActive = true,
+				isStale = false,
+			}),
+			jestExpect.objectContaining({
+				route = secondRoute,
+				index = 2,
+				isActive = false,
+				isStale = true,
+			}),
+			jestExpect.objectContaining({
+				route = thirdRoute,
+				index = 3,
+				isActive = false,
+				isStale = true,
+			}),
+		})
 	end)
 
 	local secondScreenReplacementKey =  "id-22"
@@ -230,37 +237,42 @@ return function()
 	}
 
 	it("should mark replaced scene as stale", function()
-		expect(secondState).to.never.be.equal(nil)
+		local initialScenes = ScenesReducer({}, initialState, nil, initialDescriptors)
+		local secondScenes = ScenesReducer(initialScenes, secondState, initialState, secondDescriptors)
+		local thirdScenes = ScenesReducer(secondScenes, thirdState, secondState, thirdDescriptors)
+
 		local scenes = ScenesReducer(thirdScenes, replacedSecondSceneState, thirdState, replacedSceneDescriptors)
-		expect(scenes).to.never.be.equal(initialScenes)
 		expect(#scenes).to.be.equal(4) -- replaced scene is marked stale, it is not removed
 
-		local sceneOne = scenes[1]
-		expect(sceneOne.route).to.be.equal(initialRoute)
-		expect(sceneOne.index).to.be.equal(1)
-		expect(sceneOne.isActive).to.be.equal(false)
-		expect(sceneOne.isStale).to.be.equal(false)
-
-		local sceneTwo = scenes[2]
-		expect(sceneTwo.route).to.be.equal(secondRoute)
-		expect(sceneTwo.index).to.be.equal(2)
-		expect(sceneTwo.isActive).to.be.equal(false)
-		expect(sceneTwo.isStale).to.be.equal(true)
-
-		-- because of comparison algorithm in SceneReducer.lua compareScenes
-		-- the replacement scene is after the scene it replaced because id-2 < id-22
-		local sceneTwoReplacement = scenes[3]
-		expect(sceneTwoReplacement.route).to.be.equal(secondScreenReplacementRoute)
-		-- index is still 2 because the scene index come from route index in the nextState
-		-- this is ok because filterStale in Transitioner.lua will remove the stale scene
-		expect(sceneTwoReplacement.index).to.be.equal(2)
-		expect(sceneTwoReplacement.isActive).to.be.equal(false)
-		expect(sceneTwoReplacement.isStale).to.be.equal(false)
-
-		local sceneThree = scenes[4]
-		expect(sceneThree.route).to.be.equal(thirdRoute)
-		expect(sceneThree.index).to.be.equal(3)
-		expect(sceneThree.isActive).to.be.equal(true)
-		expect(sceneThree.isStale).to.be.equal(false)
+		jestExpect(scenes).toEqual({
+			jestExpect.objectContaining({
+				route = initialRoute,
+				index = 1,
+				isActive = false,
+				isStale = false,
+			}),
+			jestExpect.objectContaining({
+				route = secondRoute,
+				index = 2,
+				isActive = false,
+				isStale = true,
+			}),
+			-- because of comparison algorithm in SceneReducer.lua compareScenes
+			-- the replacement scene is after the scene it replaced because id-2 < id-22
+			jestExpect.objectContaining({
+				route = secondScreenReplacementRoute,
+				index = 2,
+				isActive = false,
+				isStale = false,
+			}),
+			-- index is still 2 because the scene index come from route index in the nextState
+			-- this is ok because filterStale in Transitioner.lua will remove the stale scene
+			jestExpect.objectContaining({
+				route = thirdRoute,
+				index = 3,
+				isActive = true,
+				isStale = false,
+			}),
+		})
 	end)
 end
