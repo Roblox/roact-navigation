faec26dfafc75d187cc33fffc967b8c281f3e381
Anonymous Author <roflection-bot@roblox.com>
2023-01-04 15:49:32 -0800

    LUAFDN-1365 fix misleading withNavigationFocus in-file documentation (#125)

--- src/views/withNavigationFocus.lua
+++ src/views/withNavigationFocus.lua
@@ -1,28 +1,5 @@
 -- upstream https://github.com/react-navigation/react-navigation/blob/20e2625f351f90fadadbf98890270e43e744225b/packages/core/src/views/withNavigationFocus.js
---[[
-	withNavigationFocus() is a convenience function that extends withNavigation(),
-	allowing your render function (and therefor your subgraph) to access the
-	navigation context object AND an additional boolean that indicates whether or
-	not the containing screen component is in focus. For example:
-
-	function MyButtonComponent:render()
-		return withNavigationFocus(function(navigation, focused)
-			return Roact.createElement("TextButton", {
-				Visible = focused,
-				[Roact.Event.Activated] = focused and function()
-					navigation.navigate("DetailPage")
-				end,
-			})
-		end)
-	end
-
-	This is very useful when writing generic components that need to work with
-	the navigation system (e.g. preventing buttons from navigating when a screen
-	is not in focus so you don't cause double-navigation).
-
-	Note that if you ONLY need the 'navigation' context object, it is recommended
-	that you use withNavigation() for performance reasons.
-]]
+-- documentation: https://reactnavigation.org/docs/4.x/with-navigation-focus/
 local views = script.Parent
 local root = views.Parent
 local Packages = root.Parent
