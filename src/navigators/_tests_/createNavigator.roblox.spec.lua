55d0f4de78629531869c5bbb8908aa0bdee549c9
Anonymous Author <roflection-bot@roblox.com>
2024-02-15 15:35:22 -0800

    APPFDN-2682 Roact Navigation navigator may reference stale navigation state (#144)
    https://roblox.atlassian.net/browse/APPFDN-2682
    
    There is a latent bug in Roact Navigation that causes a crash in very
    extreme circumstances. The bug occurs when a navigator re-renders in the
    middle of a navigation state change. If a parent navigator receives the
    new state change before a descendant navigator receives it, but the
    child re-renders (for any reason e.g. screenProps change), then the
    child will reference the parent's mutable children table which has since
    been overwritten with the new state, thus causing a crash. The "extreme"
    circumstances come from having a deep component tree which React must
    split up over multiple frames.
    
    The fix is to refactor `getChildNavigation` to no longer reference the
    mutable table directly, and instead capture the immutable data member it
    wants within a closure.
    
    Test added which repros the bug.

--- src/navigators/_tests_/createNavigator.roblox.spec.lua
+++ src/navigators/_tests_/createNavigator.roblox.spec.lua
@@ -1,12 +1,17 @@
 return function()
 	local navigatorsModule = script.Parent.Parent
 	local RoactNavigationModule = navigatorsModule.Parent
+	local routersModule = RoactNavigationModule.routers
+	local viewsModule = RoactNavigationModule.views
 	local Packages = RoactNavigationModule.Parent
 	local jestExpect = require(Packages.Dev.JestGlobals).expect
 	local React = require(Packages.React)
 	local ReactRoblox = require(Packages.Dev.ReactRoblox)
 
 	local createNavigator = require(navigatorsModule.createNavigator)
+	local createAppContainer = require(RoactNavigationModule.createAppContainer).createAppContainer
+	local StackRouter = require(routersModule.StackRouter)
+	local StackView = require(viewsModule.RobloxStackView.StackView)
 
 	local testRouter = {
 		getScreenOptions = function()
@@ -21,6 +26,7 @@ return function()
 	end)
 
 	afterEach(function()
+		root:unmount()
 		root = nil
 	end)
 
@@ -108,5 +114,73 @@ return function()
 				}))
 			end)
 		end).toThrow('No "routes" found in navigation state')
+	end)
+
+	it("should not throw when navigating back for very deeply nested navigators", function()
+		local function NavigateTo(page)
+			return function(props)
+				React.useEffect(function()
+					props.navigation.push(page)
+				end, {})
+
+				return nil
+			end
+		end
+
+		local function GoBack(props)
+			React.useEffect(function()
+				props.navigation.goBack()
+			end, {})
+
+			return nil
+		end
+
+		local function RapidlyUpdatingStackView(props)
+			local startTime = React.useState(tick())
+			local screenProps, setScreenProps = React.useState({})
+
+			React.useEffect(function()
+				if tick() - startTime < 5 then
+					setScreenProps({}) -- Always create a new screenProps table so child always re-renders
+				end
+			end)
+
+			return React.createElement(StackView, {
+				navigation = props.navigation,
+				descriptors = props.descriptors,
+				navigationConfig = props.navigationConfig,
+				screenProps = screenProps,
+			})
+		end
+
+		local function createCustomStackNavigator(routeArray)
+			local router = StackRouter(routeArray, {})
+			return createNavigator(RapidlyUpdatingStackView, router, {})
+		end
+
+		local Navigator = createAppContainer(createCustomStackNavigator({
+			{
+				first1 = createCustomStackNavigator({
+					{
+						first2 = createCustomStackNavigator({
+							{
+								first3 = createCustomStackNavigator({
+									{
+										first4 = createCustomStackNavigator({
+											{ first5 = NavigateTo("second") },
+											{ second = GoBack },
+										}),
+									},
+								}),
+							},
+						}),
+					},
+				}),
+			},
+		}))
+
+		ReactRoblox.act(function()
+			root:render(React.createElement(Navigator))
+		end)
 	end)
 end
